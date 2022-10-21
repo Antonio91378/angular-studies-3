b60a4a4f21eafaf2798402db135ed4494e0df06a
Antonio Dias <antoniovictor200@gmail.com>
2022-10-21 15:14:18 -0300

    Estudando Angular (Programação Reativa) utilizandopipes assíncronos.
    Aprendendo a utilizar filtros de busca oordenados.
    
    validações de busca
    
    Outra coisa interessante: Salvamento de futuras reequisições repetidas,para poupar a carga no serviço tilizado

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,6 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Acoes } from './modelo/acoes';
+import { AcoesService } from './acoes.service';
+import { merge, Subscription } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
 
+const AWAIT_TYPING = 300;
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
@@ -8,6 +19,24 @@ import { FormControl } from '@angular/forms';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
+  todasAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => {
+      console.log('Fluxo Inicial');
+    })
+  );
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(AWAIT_TYPING),
+    tap(() => {
+      console.log('Fluxo do filtro.');
+    }),
+    tap(console.log),
+    filter(
+      (valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length
+    ),
+    distinctUntilChanged(), // Prioriza-se na determinada chamada de um serviço, poupá-lo, caso ele já tenha sido chamado anteriormente com os mesmos parâmetros.
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+  );
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
 
-  constructor() {}
+  constructor(private acoesService: AcoesService) {}
 }
